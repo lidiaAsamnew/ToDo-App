64058cd85a1c65c32ddf904e0f348e0d6b6ea644
lidiaAsamnew <lidia.yirbe@gmail.com>
2025-10-24 22:08:59 +0300

    add java script logic to add the task

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-alert("hello")
+document.addEventListener('DOMContentLoaded', () => {
+    const taskInput = document.getElementById('task-input');
+    const addTaskBtn = document.getElementById('add-task-btn');
+    const taskList = document.getElementById('task-list');
+    const emptyImage = document.querySelector('.empty-image');
+    const todosContainer = document.querySelector('.todos-container');
+
+
+    const toggleEmptyState = () => {
+        emptyImage.style.display = taskList.children.length === 0 ? 'block' : 'none';
+        todosContainer.style.width = taskList.children.length > 0 ? '100%' : '50%';
+    };
+
+    const addTask = (event) => {
+        event.preventDefault();
+        const taskText = taskInput.value.trim();
+        if (!taskText) {
+            return;
+        }
+        
+        const li = document.createElement('li');
+        li.innerHTML = `
+        <input type="checkbox" class="checkbox">
+        <span>${taskText}</span>
+        <div class="task-btns">
+            <button class="edit-btn"><i class = "fa-solid fa-pen"> </i></button>
+            <button class="delete-btn"><i class = "fa-solid fa-trash"> </i></button>
+         </div>  
+        `;
+
+        taskList.appendChild(li);
+        taskInput.value = '';
+        toggleEmptyState();
+    };
+    addTaskBtn.addEventListener('click', addTask);
+    taskInput.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+            addTask(e);
+        }
+    });
+});
